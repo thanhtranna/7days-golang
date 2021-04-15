132a79dab58dec75e68442d5bb1e305814b36001
Thanh Tran <tranvanthanh@gapo.com.vn>
2021-04-15 15:29:13 +0700

    add gee orm day 1

---
+++ gee-orm/day1-database-sql/go.mod
@@ -0,0 +1,5 @@
+module geeorm
+
+go 1.14
+
+require github.com/mattn/go-sqlite3 v1.14.6
---
+++ gee-orm/day2-reflect-schema/schema/schema.go
@@ -0,0 +1,38 @@
+package schema
+
+import "reflect"
+
+// Field represents a column of database
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+// Schema represents a table of database
+type Schema struct {
+	Model      interface{}
+	Name       string
+	Fields     []*Field
+	FieldNames []string
+	fieldMap   map[string]*Field
+}
+
+// GetField returns field by name
+func (schema *Schema) GetField(name string) *Field {
+	return schema.fieldMap[name]
+}
+
+// Values return the values of dest's member variables
+func (schema *Schema) RecordValues(dest interface{}) []interface{} {
+	destValue := reflect.Indirect(reflect.ValueOf(dest))
+	var fieldValues []interface{}
+	for _, field := range schema.Fields {
+		fieldValues = append(fieldValues, destValue.FieldByName(field.Name).Interface())
+	}
+	return fieldValues
+}
+
+type ITableName interface {
+	TableName() string
+}
