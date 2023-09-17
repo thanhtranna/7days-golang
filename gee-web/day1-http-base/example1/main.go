40b0448357d907a25134aab26370b97b1e83b99a
thanhtran <thanhtran@thanhtrans-iMac-Pro.local>
2023-09-17 13:56:28 +0700

    chore: add http interface handler

---
+++ gee-web/day1-http-base/example1/main.go
@@ -0,0 +1,33 @@
+package main
+
+//	Result:
+//	$ curl http://localhost:9999/
+//	URL.Path = "/"
+
+//	$ curl http://localhost:9999/hello
+//	Header["User-Agent"] = ["curl/7.87.0"]
+//	Header["Accept"] = ["*/*"]
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+// handler echoes r.URL.Path
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+// handler echoes r.URL.Header
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+}
